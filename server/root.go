64315e2885a7dffb5b5a08468ab58a8f2453121b
Jenish Jariwala <jhon.5456@gmail.com>
2026-02-21 17:00:07 +0000

    more UI setup

--- server/root.go
+++ server/root.go
@@ -6,7 +6,6 @@ import (
 	"net/http"
 
 	"jenujari/go-sbc-webapp/config"
-	"jenujari/go-sbc-webapp/html"
 
 	rtc "github.com/jenujari/runtime-context"
 )
@@ -55,35 +54,4 @@ func RunServer() {
 
 func GetServer() *http.Server {
 	return server
-}
-
-func staticHander() http.Handler {
-	return http.StripPrefix("/static/", http.FileServer(http.FS(html.GetAssetsFs())))
-}
-
-func indexhandler(w http.ResponseWriter, r *http.Request) {
-
-	tpl, err := html.GetTpl().Clone()
-	if err != nil {
-		config.GetLogger().Println("template clone failed", err)
-		http.Error(w, "internal server error", http.StatusInternalServerError)
-		return
-	}
-
-	tpl, err = tpl.ParseFS(html.GetViewsFs(), "layout.html", "index.html")
-
-	// fmt.Println(tpl.DefinedTemplates())
-
-	if err != nil {
-		config.GetLogger().Println("template not found", err)
-		http.Error(w, "template not found", http.StatusInternalServerError)
-		return
-	}
-
-	err = tpl.ExecuteTemplate(w, "layout.html", nil)
-	if err != nil {
-		config.GetLogger().Println("template execution failed", err)
-		http.Error(w, "template execution failed", http.StatusInternalServerError)
-	}
-
 }
